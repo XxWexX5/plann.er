35d8be1a6167a5c3245415b24d7ef45121cf79f9
Wevison Ramalho Silva <wevison.silva@altoqi.com.br>
2024-07-09 16:18:32 -0300

    [ FEATURE ] Creating layout page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,37 @@
+import { MapPin, Calendar, ArrowRight } from 'lucide-react'
+
+import { Logo } from './assets/images/logo'
+
 export function App() {
   return (
-    <h2 className="text-3xl font-bold underline">Hello World!</h2>
+    <div className="w-full h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
+      <div className="w-full max-w-3xl px-6 text-center space-y-10">
+        <div className='flex flex-col items-center gap-3'>
+          <Logo />
+          <p className="text-zinc-300 text-lg">Convide seus amigos e planeje sua próxima viagem!</p>
+        </div>
+
+        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+          <div className='flex items-center gap-2'>
+            <MapPin className='size-5 text-zinc-400' />
+
+            <input type="text" placeholder="Para onde você vai?" className="flex-1 bg-transparent text-lg placeholder-zinc-400 outline-0" />
+          </div>
+
+          <div className='flex items-center gap-2'>
+            <Calendar className='size-5 text-zinc-400' />
+
+            <input type="text" placeholder="Quando" className="bg-transparent w-40 outline-0" />
+          </div>
+
+          <div className='w-px h-6 bg-zinc-800'></div>
+
+          <button className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400 transition-colors'>Continuar <ArrowRight className='size-5' /></button>
+        </div>
+        
+        <p className="text-sm text-zinc-500">Ao planejar sua viagem pela plann.er você automaticamente concorda <br />
+        com nossos <a href="#" className="text-zinc-300 underline">termos de uso</a> e <a href="#" className="text-zinc-300 underline">políticas de privacidade</a>.</p>
+      </div>
+    </div>
   )
 }
