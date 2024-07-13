a0ab5a326a934ac4de00ec2329d2a1c417bb9be5
Wevison Ramalho Silva <wevison.silva@altoqi.com.br>
2024-07-13 11:16:10 -0300

    [ REFACT ] Adjust modal.

--- src/App.tsx
+++ src/App.tsx
@@ -124,11 +124,11 @@ export function App() {
                         <Dialog.Description className="text-sm text-zinc-400">
                           Os convidados irão receber e-mails para confirmar a participação na viagem.
                         </Dialog.Description>
-
-                        {emailsToInvite.length === 0 && <p className='text-sm text-zinc-400 block pt-5'>Nenhum email cadastrado!</p>}
                       </header>
                       
                       <main className='flex flex-wrap gap-2'>
+                        {emailsToInvite.length === 0 && <p className='text-sm text-zinc-400 block'>Nenhum email cadastrado!</p>}
+
                         {
                           emailsToInvite.map(email => (
                             <div key={email} className='py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2'>
@@ -186,14 +186,92 @@ export function App() {
                   </Dialog.Portal>
                 </Dialog.Root>
 
-                <button
-                  onClick={openGuestsInput}
-                  className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400 transition-colors'
-                >
-                  Confirmar viagem
+                <Dialog.Root>
+                  <Dialog.Trigger asChild>
+                    <button
+                    onClick={openGuestsInput}
+                    className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400 transition-colors'
+                  >
+                      Confirmar viagem
 
-                  <ArrowRight className='size-5' />
-                </button>
+                      <ArrowRight className='size-5' />
+                    </button>
+                  </Dialog.Trigger>
+
+                  <Dialog.Portal>
+                    <Dialog.Overlay className="bg-black/60 data-[state=open]:animate-overlayShow fixed inset-0" />
+                    
+                    <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[640px] translate-x-[-50%] translate-y-[-50%] rounded-xl bg-zinc-900 py-5 px-6 shadow-shape focus:outline-none space-y-5">
+                      <header className='space-y-2'>
+                        <Dialog.Title className="text-lg font-semibold">
+                          Selecionar convidados
+                        </Dialog.Title>
+
+                        <Dialog.Description className="text-sm text-zinc-400">
+                          Os convidados irão receber e-mails para confirmar a participação na viagem.
+                        </Dialog.Description>
+                      </header>
+                      
+                      <main className='flex flex-wrap gap-2'>
+                        {emailsToInvite.length === 0 && <p className='text-sm text-zinc-400 block'>Nenhum email cadastrado!</p>}
+
+                        {
+                          emailsToInvite.map(email => (
+                            <div key={email} className='py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2'>
+                              <span className='text-zinc-300'>{email}</span>
+
+                              <button 
+                                type='button' 
+                                onClick={() => removeEmailFromInvites(email)}
+                              >
+                                <X
+                                  className='size-4 text-zinc-400'
+                                />
+                              </button>
+                            </div>
+                          ))
+                        }
+                      </main>
+
+                      <div className='w-full h-px bg-zinc-800'></div>
+
+                      <footer>
+                        <form onSubmit={addNewEmailToInvite} className='p-2.5 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2'>
+                          <div className='px-2 flex items-center flex-1 gap-2'>
+                            <AtSign className='text-zinc-400 size-5' />
+
+                            <input
+                              type="email"
+                              name="email"
+                              placeholder="Digite o e-mail do convidado"
+                              className="w-full bg-transparent text-lg placeholder-zinc-400 outline-0"
+                            />
+                          </div>
+
+                          <button
+                            type="submit"
+                            onClick={openGuestsInput}
+                            className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center justify-center gap-2 hover:bg-lime-400 transition-colors'
+                          >
+                            Convidar
+
+                            <Plus className='size-5' />
+                          </button>
+                        </form>
+                      </footer>
+                      
+                      <Dialog.Close asChild>
+                        <button
+                          className="text-violet11 hover:bg-violet4 focus:shadow-violet7 absolute top-[0] right-[1rem] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:shadow-[0_0_0_2px] focus:outline-none"
+                          aria-label="Close"
+                        >
+                          <X className='size-5 text-zinc-100' />
+                        </button>
+                      </Dialog.Close>
+                    </Dialog.Content>
+                  </Dialog.Portal>
+                </Dialog.Root>
+                
               </div>
             )
           }
